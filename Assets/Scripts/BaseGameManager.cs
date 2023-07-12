93e7d8a2e3c1542571dbb46ad2f497b7386f03e5
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-07-12 11:41:09 +0900

    制限時間、ゲームクリア、ゲームオーバーの処理の追加

--- Assets/Scripts/BaseGameManager.cs
+++ Assets/Scripts/BaseGameManager.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BaseGameManager : MonoBehaviour
 {
+    public int timeLimit;
+    float timeCurrent;
+
+    bool gameFLG = false;
+
+    GameObject clearCanvas;
+    GameObject overCanvas;
+
+    public TextMeshProUGUI timeText;
+
     void Awake()
     {
         Arrangements();
@@ -11,16 +22,57 @@ public class BaseGameManager : MonoBehaviour
 
     void Start()
     {
-        
+        clearCanvas = GameObject.Find("ClearCanvas");
+        overCanvas = GameObject.Find("OverCanvas");
+
+        clearCanvas.SetActive(false);
+        overCanvas.SetActive(false);
+
+        timeCurrent = timeLimit;
+        timeText.text = timeCurrent.ToString("000");
     }
 
     void Update()
     {
+        if (gameFLG)
+        {
+            timeCurrent -= Time.deltaTime;
+
+            timeText.text = timeCurrent.ToString("000");
+            if (timeCurrent >= timeLimit)
+            {
+                TimeUp();
+            }
+        }
         
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Arrangements();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            gameFLG = true;
+        }
     }
 
     virtual public void Arrangements()
     {
+        
+    }
 
+    virtual public void TimeUp()
+    {
+        GameOver();
+    }
+
+    public void GameClear()
+    {
+        clearCanvas.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        overCanvas.SetActive(true);
     }
 }
