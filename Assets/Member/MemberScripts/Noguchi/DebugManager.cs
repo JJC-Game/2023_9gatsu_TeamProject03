da1572f48721dac985bb2c02e1f8784f33b0266d
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-07-13 15:11:39 +0900

    デバッグ用のスクリプトの追加

---
+++ Assets/Member/MemberScripts/Noguchi/DebugManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugManager : MonoBehaviour
+{
+    public GameObject scroll;
+    RectTransform rect;
+
+    void Start()
+    {
+        rect = scroll.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rect.localPosition += new Vector3(0, 10, 0);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            rect.localPosition += new Vector3(0, -10, 0);
+        }
+    }
+}
