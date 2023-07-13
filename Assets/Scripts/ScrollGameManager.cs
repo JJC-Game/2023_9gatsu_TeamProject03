05baa3a55bea842999c4bda105e7460ca5f64c57
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-07-13 14:02:06 +0900

    スクロールゲームの処理とシーンの編集

--- Assets/Scripts/ScrollGameManager.cs
+++ Assets/Scripts/ScrollGameManager.cs
@@ -1,17 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScrollGameManager : BaseGameManager
 {
+    //使用するイラスト
+    Sprite sprite_Target;
+    //イラストの数（イラストの通し番号の最も大きい数）
+    public int spriteMin;
+    public int spriteMax;
+
+    public int iconMin = 6;
+
+    GameObject[] target;
+
+    public Image targetPicture;
+
     AimController aim;
 
     float aimCount;
     public float aimGoal;
 
+    int correctCount;
+    public int correctGoal;
+
+    public TextMeshProUGUI questionNoText;
+
     public override void Arrangements()
     {
+        IconSet();
+
         aim = GameObject.Find("Aim").GetComponent<AimController>();
+        correctCount = 0;
+        questionNoText.text = (correctCount + 1).ToString("00");
+
+        target = GameObject.FindGameObjectsWithTag("Target");
+
+        RandomChange();
+    }
+
+    void IconSet()
+    {
+        GameObject iconParent = GameObject.Find("BackGround_Scroll");
+
+        int childCount = iconParent.transform.childCount;
+        GameObject[] icon = new GameObject[childCount];
+
+        for (int n = 0; n < childCount; n++)
+        {
+            Transform childTransform = iconParent.transform.GetChild(n);
+            GameObject childObject = childTransform.gameObject;
+
+            icon[n] = childObject;
+        }
+
+        int randomNo = Random.Range(iconMin, childCount);
+
+        for (int i = randomNo; i < childCount; i++)
+        {
+            icon[i].SetActive(false);
+        }
+    }
+
+    void RandomChange()
+    {
+        TargetReset();
+
+        int randomTarget = Random.Range(0, target.Length);
+        int randomTexture = Random.Range(spriteMin, spriteMax + 1);
+
+        sprite_Target = Resources.Load<Sprite>("ProjectAssets/GameIcon/Icon_" + randomTexture);
+
+        target[randomTarget].SetActive(true);
+
+        Image targetImage = target[randomTarget].GetComponent<Image>();
+        targetImage.sprite = sprite_Target;
+        targetPicture.sprite = sprite_Target;
+    }
+
+    void TargetReset()
+    {
+        for (int i = 0; i < target.Length; i++)
+        {
+            target[i].SetActive(false);
+        }
     }
 
     public override void UpdatePlus()
@@ -22,7 +96,19 @@ public class ScrollGameManager : BaseGameManager
 
             if (aimCount >= aimGoal)
             {
-                GameClear();
+                if (gameFLG)
+                {
+                    correctCount++;
+
+                    if (correctCount >= 3)
+                    {
+                        GameClear();
+                    }
+                    else
+                    {
+                        questionNoText.text = (correctCount + 1).ToString("00");
+                    }
+                }
             }
         }
         else
