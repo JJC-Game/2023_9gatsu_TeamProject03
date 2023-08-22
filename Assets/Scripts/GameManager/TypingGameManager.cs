f23fdb03f5a96d1f1a7af16f75ba5960f6d348b3
NoguchiEikichi <aymek.5aikichi@icloud.com>
2023-08-22 13:51:24 +0900

    Update TypingGameManager.cs

--- Assets/Scripts/GameManager/TypingGameManager.cs
+++ Assets/Scripts/GameManager/TypingGameManager.cs
@@ -1,15 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class TypingGameManager : BaseGameManager
 {
     string answerWard;
-    string[] questionWard;
+    public List<string> fixDataList;
 
     public override void Arrangements()
     {
-        
+        LoadFixData();
+    }
+
+    void LoadFixData()
+    {
+        TextAsset csvFile;
+        csvFile = Resources.Load("FixData/ItemFixData") as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+
+        while (reader.Peek() > -1)
+        {
+            string line = reader.ReadLine();
+            string[] elementArray = line.Split(',');
+            Debug.Log(line);
+
+            fixDataList.Add(elementArray[0]);
+            //            csvDatas.Add(line.Split(',')); // リストに入れる
+        }
+
+
     }
 
     public void Decision(string input)
