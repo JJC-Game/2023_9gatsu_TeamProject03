b51516a2b47f87987e7e5f438a845b5d8bbea789
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-08-23 15:12:16 +0900

    フリックゲームの処理の追加

---
+++ Assets/Scripts/GameManager/FlickGameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickGameManager : BaseGameManager
+{
+    public override void Arrangements()
+    {
+        
+    }
+
+    public override void UpdatePlus()
+    {
+        
+    }
+}
