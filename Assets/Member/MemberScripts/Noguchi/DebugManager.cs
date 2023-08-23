12e9c7bf4ee1f36d62b6ff5e0a7cf5919b08566c
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-08-23 14:31:30 +0900

    スクロールゲームの修正

--- Assets/Member/MemberScripts/Noguchi/DebugManager.cs
+++ Assets/Member/MemberScripts/Noguchi/DebugManager.cs
@@ -8,9 +8,12 @@ public class DebugManager : MonoBehaviour
     public GameObject scroll;
     RectTransform rect;
 
+    ScrollGameManager sgm;
+
     void Start()
     {
         rect = scroll.GetComponent<RectTransform>();
+        sgm = GameObject.Find("ScrollManager").GetComponent<ScrollGameManager>();
     }
 
     void Update()
@@ -22,6 +25,11 @@ public class DebugManager : MonoBehaviour
         else if(Input.GetKey(KeyCode.S))
         {
             rect.localPosition += new Vector3(0, -10, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            sgm.GameStart();
         }
     }
 }
