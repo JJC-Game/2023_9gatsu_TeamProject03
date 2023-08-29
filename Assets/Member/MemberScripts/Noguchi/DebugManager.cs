0e7d6c21e79aa9f1cdc1eed5695ce998cb3ffa36
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-08-29 12:12:03 +0900

    スワイプのゲームの処理の追加

--- Assets/Member/MemberScripts/Noguchi/DebugManager.cs
+++ Assets/Member/MemberScripts/Noguchi/DebugManager.cs
@@ -5,31 +5,5 @@ using UnityEngine.UI;
 
 public class DebugManager : MonoBehaviour
 {
-    public GameObject scroll;
-    RectTransform rect;
-
-    ScrollGameManager sgm;
-
-    void Start()
-    {
-        rect = scroll.GetComponent<RectTransform>();
-        sgm = GameObject.Find("ScrollManager").GetComponent<ScrollGameManager>();
-    }
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            rect.localPosition += new Vector3(0, 10, 0);
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            rect.localPosition += new Vector3(0, -10, 0);
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            sgm.GameStart();
-        }
-    }
+    
 }
