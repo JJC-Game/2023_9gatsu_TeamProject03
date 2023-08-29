7cb0aba52445774e79197f512badef0346f28e8f
NoguchiEikichi <128008974+NoguchiEikichi@users.noreply.github.com>
2023-08-29 15:17:17 +0900

    メニュー画面にスコアを表示する処理の追加

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMeshProUGUI>();
+        text.text = PlayerPrefs.GetInt("", 0).ToString("0000");
+    }
+}
