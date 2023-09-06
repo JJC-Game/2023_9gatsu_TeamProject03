3e6234f3297e5796369a18bab75f777391d65155
BabaRui <128009067+BabaRui@users.noreply.github.com>
2023-09-06 12:09:15 +0900

    TapEffectの処理の作成

---
+++ Assets/Member/MemberScripts/Baba/EffectDelete.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDelete : MonoBehaviour
+{
+    public float growthDuration = 1.5f; // 成長にかける時間
+    public float fadeDuration = 1.5f;   // 色を薄くする時間
+
+    private Material material;
+    private Color initialColor;
+    private float startTime;
+    public float targetScale = 1.2f; // 目標のサイズ
+
+    private float initialScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material; // オブジェクトのマテリアルを取得
+        initialColor = material.color; // 初期色を保存
+        startTime = Time.time; // 開始時間を記録
+
+        // 1.5秒後にDestroyを呼び出すコルーチンを開始
+        StartCoroutine(DestroyAfterDelay(growthDuration));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 現在の経過時間を計算
+        float elapsedTime = Time.time - startTime;
+
+        // 目標色に達するまで色を変更
+        if (elapsedTime < fadeDuration)
+        {
+            float t = elapsedTime / fadeDuration; // 色のフェードの進捗度合い
+            Color newColor = Color.Lerp(initialColor, Color.clear, t);
+            material.color = newColor;
+            float newScale = Mathf.Lerp(initialScale, targetScale, t);
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+        }
+    }
+
+    // 指定した時間を待ってからGameObjectを削除するコルーチン
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Member/MemberScripts/Baba/TapEffect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class TapEffect : MonoBehaviour
+{
+    public GameObject effectParent; // エフェクトの親オブジェクトを指定
+    private GameObject effectInstance;
+
+    private void Update()
+    {
+        // マウスクリックを検出
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePosition = Input.mousePosition;//クリックした場所に取得
+            mousePosition.z = 8.8f; // Z座標を設定
+            effectInstance = GameObject.Instantiate(EffectManager.Instance.playerFX[0], Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+
+            // 生成したエフェクトを親オブジェクトの子オブジェクトにする
+            if (effectParent != null)
+            {
+                effectInstance.transform.SetParent(effectParent.transform);
+            }
+        }
+
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);//タッチした場所に取得
+
+            // タップが開始された瞬間（TouchPhase.Began）のみオブジェクトを生成
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 8.8f)); // Z座標を設定
+                effectInstance = GameObject.Instantiate(EffectManager.Instance.playerFX[0], touchPosition, Quaternion.identity);
+
+                // 生成したエフェクトを親オブジェクトの子オブジェクトにする
+                if (effectParent != null)
+                {
+                    effectInstance.transform.SetParent(effectParent.transform);
+                }
+            }
+        }
+    }
+}
