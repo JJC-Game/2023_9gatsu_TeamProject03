9d8a4a8e55b8ec57ee5191e9e4ae57c26772491b
BabaRui <128009067+BabaRui@users.noreply.github.com>
2023-09-06 15:20:06 +0900

    ドラッグゲームのスキップボタン実装と修正

--- Assets/Member/MemberPrefabs/Baba/DragCuizu/Script/AnotherScript.cs
+++ Assets/Member/MemberPrefabs/Baba/DragCuizu/Script/AnotherScript.cs
@@ -82,5 +82,59 @@ public class AnotherScript : MonoBehaviour
         }
      
     }
+    public void skip()
+    {
+        if (baseManager.inGameEnable == true)
+        {
+            okTM = true;
+            // GetChildTMPro スクリプトがアタッチされたオブジェクトを取得
+            GameObject getChildTMProObject = GameObject.Find("T");
+            GameObject getAnser = GameObject.Find("GameManager");
+            GetChildTMPro[] getChildTMProScripts = getChildTMProObject.GetComponentsInChildren<GetChildTMPro>();
+            DragFixData getAnsers = getAnser.GetComponent<DragFixData>();
+            int trueCount = 0;
+            int okCont = 0;
+            // 各子オブジェクトの condition プロパティにアクセス
+            foreach (GetChildTMPro getChildTMProScript in getChildTMProScripts)
+            {
+                bool isConditionTrue = getChildTMProScript.condition;
 
+                if (isConditionTrue)
+                {
+                    trueCount++;
+                }
+            }
+            // 各子オブジェクトの condition プロパティにアクセス
+            foreach (GetChildTMPro getChildTMProScript in getChildTMProScripts)
+            {
+                bool isConditionTrue = getChildTMProScript.ok;
+
+                if (isConditionTrue)
+                {
+                    okCont++;
+                }
+            }
+            List<string> anser = new List<string>();
+            anser = getAnsers.Options;
+
+
+                Debug.Log("正かい");
+                ok = true;
+                okTM = true;
+                for (int i = 0; i < getChildTMProScripts.Length; i++)
+                {
+                    getChildTMProScripts[i].DragID = 99;
+                    getChildTMProScripts[i].ID = 88;
+                    getChildTMProScripts[i].condition = false;
+                    getChildTMProScripts[i].ok = false;
+                    Debug.Log("リセット");
+                   // baseManager.AddScore();
+                }
+
+            // true の数を表示
+            Debug.Log("文字の数 " + trueCount);
+            Debug.Log("合ってる数 " + okCont);
+        }
+
+    }
 }
